e4d11b72cb34044df08bc60d229dee0b8f1215d8
ZanClifton <96394256+ZanClifton@users.noreply.github.com>
2022-04-03 00:03:36 +0100

    minor tidying up

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -20,3 +20,4 @@ exports.getUser = async (req, res, next) => {
         next(err);
     };
 };
+
--- routes/users-router.js
+++ routes/users-router.js
@@ -1,8 +1,8 @@
-const { getUsers, getUser } = require("../controllers/users.controller");
+const { getUsers, getUser, getUserArticles } = require("../controllers/users.controller");
 const userRouter = require("express").Router();
   
 userRouter.get("/api/users", getUsers);
 userRouter.get("/api/users/:username", getUser);
-
+userRouter.get("/api/users/:username/articles", getUserArticles);
   
 module.exports = userRouter;
